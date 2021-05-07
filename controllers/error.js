62359c20876d13194f0cfb0871e0964998483515
JoshEthan <joshuaeris@gmail.com>
2021-05-06 18:12:29 -0600

    Realized a different way to organize the folder.

---
+++ controllers/error.js
@@ -0,0 +1,5 @@
+exports.get404 = (req, res, next) => {
+    res.render('pages/404', {
+        title: '404 - Page Not Found', 
+        path: req.url});
+  };
---
+++ models/products.js
@@ -0,0 +1,20 @@
+// module.exports = function Product() {
+
+// }
+
+const products = [];
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+
+    }
+
+    save() {
+        products.push(this);
+    }
+
+    static fetchAll() {
+        return products;
+    }
+}
