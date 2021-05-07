d2422ada389262dcdf43a2ddcfdc9438c632973a
JoshEthan <joshuaeris@gmail.com>
2021-05-06 18:12:10 -0600

    Realized a different way to organize the folder.

--- index.js
+++ index.js
@@ -13,8 +13,6 @@
 // Our initial setup (package requires, port number setup)
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require("fs");
-const ejs = require("ejs");
 
 const path = require('path');
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
@@ -28,16 +26,13 @@ const ta03Routes = require('./routes/team_activities/ta03');
 const ta04Routes = require('./routes/team_activities/ta04'); 
 const prove01Routes = require('./routes/prove_activities/prove01'); 
 const prove02Routes = require('./routes/prove_activities/prove02'); 
+const prove03Routes = require('./routes/prove_activities/prove03');
+const errorController = require('./controllers/error'); 
 
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
    .set('view engine', 'ejs')
-   // For view engine as Pug
-   //.set('view engine', 'pug') // For view engine as PUG. 
-   // For view engine as hbs (Handlebars)
-   //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
-   //.set('view engine', 'hbs')
    .use(bodyParser({extended: false})) // For parsing the body of a POST
    .use('/ta01', ta01Routes)
    .use('/ta02', ta02Routes) 
@@ -45,12 +40,12 @@ app.use(express.static(path.join(__dirname, 'public')))
    .use('/ta04', ta04Routes)
    .use('/prove01', prove01Routes)
    .use('/prove02', prove02Routes)
+   .use('/prove03', prove03Routes)
    .get('/', (req, res, next) => {
-     // This is the primary index, always handled last. 
-     res.render('pages/index', {title: 'Welcome to my CSE341 repo', path: '/'});
+      // This is the primary index, always handled last. 
+      res.render('pages/index', {
+        title: 'Welcome to my CSE341 repo',
+        path: '/'});
     })
-   .use((req, res, next) => {
-     // 404 page
-     res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
-   })
+   .use(errorController.get404)
    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
