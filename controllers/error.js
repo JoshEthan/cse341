fd9fbc4fcbd8d66749041a074d8e6a3be782b82f
JoshEthan <joshuaeris@gmail.com>
2021-05-08 23:19:22 -0600

    Prove 03 - Incomplete

--- controllers/error.js
+++
@@ -1,5 +0,0 @@
-exports.get404 = (req, res, next) => {
-    res.render('pages/404', {
-        title: '404 - Page Not Found', 
-        path: req.url});
-  };
--- index.js
+++ index.js
@@ -20,31 +20,30 @@ const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localho
 const app = express();
 
 // Route setup.
-const ta01Routes = require('./routes/team_activities/ta01');
-const ta02Routes = require('./routes/team_activities/ta02');
-const ta03Routes = require('./routes/team_activities/ta03'); 
-const ta04Routes = require('./routes/team_activities/ta04'); 
-const prove01Routes = require('./routes/prove_activities/prove01'); 
-const prove02Routes = require('./routes/prove_activities/prove02'); 
-const prove03Routes = require('./routes/prove_activities/prove03');
-const errorController = require('./controllers/error'); 
+const userRoutes = require('./routes/userRoutes');
+const shopRoutes = require('./routes/shopRoutes'); 
+// const ta04Routes = require('./routes/ta04'); 
+// const prove01Routes = require('./routes/prove01'); 
+// const prove02Routes = require('./routes/prove02'); 
+// const prove03Routes = require('./routes/prove03');
+const errorController = require('./controllers/errorController'); 
 
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
    .set('view engine', 'ejs')
    .use(bodyParser({extended: false})) // For parsing the body of a POST
-   .use('/ta01', ta01Routes)
-   .use('/ta02', ta02Routes) 
-   .get('/ta03', ta03Routes.processJson) 
-   .use('/ta04', ta04Routes)
-   .use('/prove01', prove01Routes)
-   .use('/prove02', prove02Routes)
-   .use('/prove03', prove03Routes)
+   .use('/userRoutes', userRoutes) 
+   .use('/shopRoutes', shopRoutes) 
+   // .get('/ta03', ta03Routes.processJson) 
+   // .use('/ta04', ta04Routes)
+   // .use('/prove01', prove01Routes)
+   // .use('/prove02', prove02Routes)
+   // .use('/prove03', prove03Routes)
    .get('/', (req, res, next) => {
       // This is the primary index, always handled last. 
       res.render('pages/index', {
-        title: 'Welcome to my CSE341 repo',
+        title: 'Welcome to BYU-I Surplus Store!',
         path: '/'});
     })
    .use(errorController.get404)
