87a7853a58f9b55733fa7e1ea84dc5b64be2f869
JoshEthan <joshuaeris@gmail.com>
2021-05-13 11:27:25 -0600

    First commit

---
+++ models/product.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+    }
+  });
+
+module.exports = mongoose.model('Product', productSchema);
+
+
+
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = require('../util/database');
+
+// const Product = sequelize.define('product', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   },
+//   title: Sequelize.STRING,
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false
+//   },
+//   imageUrl: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   }
+// });
+
+// module.exports = Product;
